80710d7b427935961c3a3dacec219a5467de9d3a
Florient Leonard <florient_leonard@hotmail.com>
2023-03-03 10:46:30 +0400

    Ajout des premières routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './components/Header'
+import Error from './components/Error'
 import './styles/index.css';
-import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
