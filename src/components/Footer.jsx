2b78d4b4ad0480f96a7ca89820336870721e6b0e
Florient Leonard <florient_leonard@hotmail.com>
2023-03-07 17:43:03 +0400

    Finalisation du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import logo from '../assets/Logo_Footer.png'
+
+
 function Footer() {
     return (
-        <p>Ceci est le footer</p>
+        <footer className="footer">
+            <Link to="/">
+                <img src={logo} alt="Logo de Kasa" />
+            </Link>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
     )
 }
 
