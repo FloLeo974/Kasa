98b993e9f4466f23cb0a23a2c3d42ec17c060052
Florient Leonard <florient_leonard@hotmail.com>
2023-03-10 18:42:43 +0400

    Ajout du composant Card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,30 @@
-import Banner from "../components/Banner"
 import BannerImg from "../assets/Banner_Home.jpg"
-import datasLogements from "../datas/logements.json";
-import { Link } from "react-router-dom";
+import Banner from "../components/Banner"
+import Card from "../components/Card"
 
 function Home() {
     return(
         <main className="main__home">
             <Banner image={BannerImg} texte="Chez vous, partout et ailleurs"/>
-
-            <div className="cards">
-                {datasLogements.map(({ id, title, cover }) => (    
-                    <Link to={`Logement/:${id}`} className="card" key={id} title={title} id={id} cover={cover}>
-                        <img src={cover} alt="" className="card__img" />
-                        <p className="cards__title">{title}</p>
-                    </Link>
-                ))}
-            </div>
+            <Card />
         </main>
     )
 }
 
 export default Home
+
+/* 2ème solution:
+
+import datasLogements from "../datas/logements.json"
+import { Link } from "react-router-dom";
+
+            <div className="cards">
+                {datasLogements.map(( logement ) => (    
+                    <Link to={`Logement/:${logement.id}`} className="card" key={logement.id}>
+                        <img src={logement.cover} alt={logement.title}/>
+                        <p>{logement.title}</p>
+                    </Link>
+                ))}
+            </div>
+
+*/
