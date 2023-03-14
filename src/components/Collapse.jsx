469a386193d3102906e0c4873303b4c3c39998df
Florient Leonard <florient_leonard@hotmail.com>
2023-03-14 20:47:54 +0400

    Réutilisation des Collapses pour les logements useNavigate pour les erreurs d'id

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react"
 import arrow from "../assets/Arrow.svg"
 
-// en mode fonction
 function Collapse({title, texte}) {
     const [open, setOpen] = useState(false)
     return (
             <div className="collapse">
-                <div className="collapse__title" onClick={() => open === true ? setOpen(false) : setOpen(true)}/* true */>
+                <div className="collapse__title" onClick={() => open === true ? setOpen(false) : setOpen(true)}>
                     <div className="collapse__title__texte">{title}</div>
                     <div className={`collapse__title__arrow--${open}`}>
-                        <img src={arrow} alt="Icône de flèche"/>
+                        <img className="collapse__title__arrow" src={arrow} alt="Icône de flèche"/>
                     </div>
                 </div>
                 {open === true && <div className="collapse__texte">{texte}</div>}
--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -1,12 +1,38 @@
 import { useEffect } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import Collapse from "../components/Collapse"
+import accomodations from "../datas/accomodations_list.json"
 
 function Accomodation() {
+    const navigate = useNavigate()
+    const params = useParams()
+    const accomodation = accomodations.find( element => element.id === params.id)
 
     useEffect(() => {
-        document.title = "Kasa - Logement" // à paramétrer avec le nom du logement
+        if (!accomodation) {
+           navigate("/error");
+        }
+        else {
+            document.title = `Kasa - ${accomodation.title}`
+        }
     })
 
-    return <div>Logement</div>
+    return(
+        <main className="main__accomodation">
+            <div className="collapses collapses--small">
+                <Collapse 
+                    title="Description" 
+                    texte={accomodation.description}
+                />
+                <Collapse 
+                    title="Équipements" 
+                    texte={accomodation.equipments.map((equipments, index) => (
+                        <div key={index}>{equipments}</div>
+                    ))}
+                />
+            </div>
+        </main>
+    )
 }
 
 export default Accomodation
