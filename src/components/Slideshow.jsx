0702556e7575b4bc1301d356f89e14fc952bfbc8
Florient Leonard <florient_leonard@hotmail.com>
2023-03-16 17:10:03 +0400

    Non affichage des bullet points si une seule photo

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -2,23 +2,26 @@ import { useState } from "react"
 import ArrowSlides from "../assets/Arrow_Slides.svg"
 
 function Slideshow({ pictures }) {
-    const [slide, setSlide] = useState(0);
+    const [slide, setSlide] = useState(0)
     const previousSlide = () => {setSlide(slide === 0 ? pictures.length -1 : slide -1)}
     const nextSlide = () => {setSlide(slide === pictures.length -1 ? 0 : slide + 1)}
-  
+    const noSinglePicture = pictures?.length > 1
+
     return (
       <div className="slideshow">
         <img className="slideshow__img" src={pictures?.[slide]} alt="logement"/>
 
-        {pictures?.length > 1 && (
+        {noSinglePicture && (
           <img className="arrow_slides arrow_left" src={ArrowSlides} alt="flèche vers la gauche" onClick={previousSlide}/>
         )}
 
-        {pictures?.length > 1 && (
+        {noSinglePicture && (
           <img className="arrow_slides arrow_right" src={ArrowSlides} alt="flèche vers la droite" onClick={nextSlide}/>
         )}
 
-        <p className="bullet_points">{slide +1}/{pictures?.length}</p>
+        {noSinglePicture && (
+          <p className="bullet_points">{slide +1}/{pictures?.length}</p>
+        )}
       </div>
     )
   }
