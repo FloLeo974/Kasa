11fe2204183a2a6e722ddf17ff42e91151197226
Florient Leonard <florient_leonard@hotmail.com>
2023-03-17 14:50:19 +0400

    Optimisation du composant Slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -2,28 +2,23 @@ import { useState } from "react"
 import ArrowSlides from "../assets/Arrow_Slides.svg"
 
 function Slideshow({ pictures }) {
-    const [slide, setSlide] = useState(0)
-    const previousSlide = () => {setSlide(slide === 0 ? pictures.length -1 : slide -1)}
-    const nextSlide = () => {setSlide(slide === pictures.length -1 ? 0 : slide + 1)}
-    const noSinglePicture = pictures?.length > 1
+  const [slide, setSlide] = useState(0)
+  const previousSlide = () => {setSlide(slide === 0 ? pictures.length -1 : slide -1)}
+  const nextSlide = () => {setSlide(slide === pictures.length -1 ? 0 : slide + 1)}
 
-    return (
-      <div className="slideshow">
-        <img className="slideshow__img" src={pictures?.[slide]} alt="logement"/>
+  return (
+    <div className="slideshow">
+      <img className="slideshow__img" src={pictures?.[slide]} alt="logement"/>
 
-        {noSinglePicture && (
+      {pictures?.length > 1 && (
+        <div>
           <img className="arrow_slides arrow_left" src={ArrowSlides} alt="flèche vers la gauche" onClick={previousSlide}/>
-        )}
-
-        {noSinglePicture && (
           <img className="arrow_slides arrow_right" src={ArrowSlides} alt="flèche vers la droite" onClick={nextSlide}/>
-        )}
-
-        {noSinglePicture && (
           <p className="bullet_points">{slide +1}/{pictures?.length}</p>
-        )}
-      </div>
-    )
-  }
-  
-  export default Slideshow
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Slideshow
